dd6ff1dc0bd688fce667a80c2a332f347dfa811b
vishnumishra <vishnu143mishra@gmail.com>
2015-03-07 10:19:58 +0530

    -v.3 is completed and working for both direction

--- Paths.java
+++ Paths.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class MyDataBase{
 	public static Map<String,ArrayList<String>> initDataBase(){
-		DataBase db = new DataBase(); 
-		db.insert("Bangalore","Singapore");
-		db.insert("Singapore","Dubai");
+		DataBase db = new DataBase();
+        db.insert("Bangalore", "Singapore");
+        db.insert("Singapore","Dubai");
 		db.insert("Singapore","Seoul");
 		db.insert("Seoul","Beijing");
 		db.insert("Beijing","Tokyo");
@@ -24,24 +24,25 @@ class isPresent{
 }
 
 public  class Paths{
-	static  Map<String,ArrayList<String>> db = MyDataBase.initDataBase();
-    private static Set<String> intermediateCitys = new HashSet<String>();
-    private static boolean hasPath(String src, String dest){
+    private Deque<String> path = new ArrayDeque<String>();
+    Map<String,ArrayList<String>> db = MyDataBase.initDataBase();
+    private boolean hasPath(String src, String dest){
         ArrayList<String> list = db.get(src);
-        intermediateCitys.add(src);
-        if(list == null) return false;
-        if (list.contains(dest) || src == dest){
-            intermediateCitys.add(dest);
-            return true;
-        }
-        for (String city:list) {
-            intermediateCitys.add(city);
-            if (hasPath(city, dest)) return true;   
-        }
-    	return false;
+        if(list != null){
+            if (list.contains(dest)){
+                return true;
+            }
+            for (String city:list) {
+                if (hasPath(city, dest)){   
+                    path.add(city);
+                    return true;   
+                } 
+            }
+        } 
+        return false;
     }
 
-    public  static boolean hasAnyPath(String src, String dest)throws Exception{
+    public boolean hasAnyPath(String src, String dest)throws Exception{
         if(!isPresent.city(db,src))
             throw new Exception("No city named \""+src+"\" in database");
         if(!isPresent.city(db,dest))
@@ -49,9 +50,21 @@ public  class Paths{
         return (hasPath(src,dest))?true:hasPath(dest,src);
     }
 
-    public static String getPath(String src,String dest)throws Exception{
-    	if(hasAnyPath(src,dest))
-    		return intermediateCitys.toString();
+    public String getPath(String src,String dest)throws Exception{
+        path.clear();
+        String result =src;
+        if(hasPath(src,dest)){
+            for(Iterator descItr = path.descendingIterator();descItr.hasNext();) {
+                result += "==>"+descItr.next();
+             }
+            return result+"==>"+dest;
+        }
+        if(hasPath(dest,src)){
+            for(Iterator itr = path.iterator();itr.hasNext();) {
+                result += "==>"+itr.next();
+             }
+            return result+"==>"+dest;
+        }
     	return "Sorry no path";
     }
 }
--- PathsTest.java
+++ PathsTest.java
@@ -3,32 +3,91 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PathsTest{
-
 	@Test
 	public void paths_tell_that_are_any_path_btwn_Bangalore_to_Singapore()throws Exception{
-		boolean predicate = Paths.hasAnyPath("Bangalore","Singapore");
+		Paths p= new Paths();
+		boolean predicate = p.hasAnyPath("Bangalore","Singapore");
 		assertEquals(true,predicate);
 	}
 	@Test
+	public void hasAnyPath_tell_that_the_Gugrat_is_not_in_dataBase()throws Exception{
+		Paths p= new Paths();
+		try{
+			boolean predicate = p.hasAnyPath("Gugrat","Singapore");
+		}catch(Exception e){
+			assertEquals(e.getMessage(),"No city named \""+"Gugrat"+"\" in database");
+		}
+	}
+	@Test
+	public void hasAnyPath_tell_that_the_Japan_is_not_in_dataBase()throws Exception{
+		Paths p= new Paths();
+		try{
+			boolean predicate = p.hasAnyPath("Bangalore","Japan");
+		}catch(Exception e){
+			assertEquals(e.getMessage(),"No city named \""+"Japan"+"\" in database");
+		}
+	}
+
+	@Test
 	public void paths_tell_that_their_any_path_btwn_Bangalore_to_Tokyo()throws Exception{
-		boolean predicate = Paths.hasAnyPath("Bangalore","Tokyo");
+		Paths p= new Paths();
+		boolean predicate = p.hasAnyPath("Bangalore","Tokyo");
 		assertEquals(true,predicate);
 	}
 	@Test
 	public void paths_tell_that_their_any_path_btwn_Tokyo_to__Bangalore()throws Exception{
-		boolean predicate = Paths.hasAnyPath("Tokyo","Bangalore");
+		Paths p= new Paths();
+		boolean predicate = p.hasAnyPath("Tokyo","Bangalore");
 		assertEquals(true,predicate);
 	}
 
 	@Test
 	public void paths_tell_that_their_any_path_From_Bangalore_to_Finland()throws Exception{
-		boolean predicate = Paths.hasAnyPath("Bangalore","Finland");
+		Paths p= new Paths();
+		boolean predicate = p.hasAnyPath("Bangalore","Finland");
 		assertEquals(true,predicate);
 	}
 	@Test
 		public void paths_tell_that_their_any_path_From_Finland_to_Bangalore()throws Exception{
-		boolean predicate = Paths.hasAnyPath("Finland","Bangalore");
+		Paths p= new Paths();
+		boolean predicate = p.hasAnyPath("Finland","Bangalore");
 		assertTrue(predicate);
 	}
-
+    @Test
+        public  void getPath_give_the_path_between_Finland_to_Singapore()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Finland","Singapore");
+        assertEquals("Finland==>Dubai==>Singapore",path);
+    }
+    @Test
+        public  void getPath_give_the_path_between_Singapore_to_Finland()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Singapore","Finland");
+        assertEquals("Singapore==>Dubai==>Finland",path);
+    }
+    @Test
+        public  void getPath_give_the_path_between_Bangalore_to_singapore()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Bangalore","Singapore");
+        assertEquals("Bangalore==>Singapore",path);
+    }
+    @Test
+        public  void getPath_give_the_path_between_singapore_to_Bangalore()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Singapore","Bangalore");
+        assertEquals("Singapore==>Bangalore",path);
+    }
+    @Test
+        public  void getPath_give_the_path_between_Tokyo_to_Bangalore()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Tokyo","Bangalore");
+        assertEquals("Tokyo==>Beijing==>Seoul==>Singapore==>Bangalore",path);
+    }
+    @Test
+        public  void getPath_give_the_path_between_Bangalore_to_Tokyo()throws Exception{
+		Paths p= new Paths();
+        String path = p.getPath("Bangalore","Tokyo");
+        assertEquals("Bangalore==>Singapore==>Seoul==>Beijing==>Tokyo",path);
+    }
+    
 }
